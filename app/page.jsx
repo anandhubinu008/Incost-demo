5e022590ba7aba096ab127f2f256cb5ed92dfa6d
anandhubinu008 <postboxnumber008@gmail.com>
2025-08-17 17:19:42 +0530

    Changes

--- app/page.jsx
+++ app/page.jsx
@@ -9,13 +9,13 @@ const page = () => {
   const router = useRouter();
 
   return (
-    <main className="w-full relative items-center flex px-15 justify-center bg-no-repeat bg-cover h-[100vh] ">
+    <main className="w-full relative items-center flex px-15 justify-center items-center bg-no-repeat bg-cover h-[100dvh] ">
       <div className='z-20'>
-        <h1 className='text-8xl font-bold'>The Art of Stunning Interior</h1>
+        <h1 className='lg:text-8xl font-bold'>The Art of Stunning Interior</h1>
         <div className='m-auto'>
           <p className='pt-5 text-4xl font-semibold'><span className='text-amber-500'>Calculate the cost of your work instantly, </span> based on materials and products choices.</p>
         </div>
-        <div className='mt-4 text-xl w-[50%]'>
+        <div className='mt-4 text-xl lg:w-[50%]'>
           <p className='text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere placeat saepe similique aliquid rerum neque quo cupiditate voluptatum doloribus reprehenderit enim, totam quaerat ipsum inventore eaque necessitatibus eos temporibus quas.</p>
         </div>
         <div className='mt-8'>
@@ -23,7 +23,7 @@ const page = () => {
           <button className='border px-6 py-3 border-gray-300 cursor-pointer' onClick={() => router.push('/dashboard')}>Try Now</button>
         </div>
       </div>
-      <div className="absolute  z-10 w-[50%] bg-cover h-full right-0 top-0 bg-[url('https://images.pexels.com/photos/1571458/pexels-photo-1571458.jpeg?cs=srgb&dl=pexels-fotoaibe-1571458.jpg&fm=jpg')]">
+      <div className="absolute  z-10 lg:w-[50%] bg-cover h-full right-0 top-0 bg-[url('https://images.pexels.com/photos/1571458/pexels-photo-1571458.jpeg?cs=srgb&dl=pexels-fotoaibe-1571458.jpg&fm=jpg')]">
 
       </div>
     </main>
